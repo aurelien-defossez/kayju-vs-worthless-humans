758d7a31a44bd8a6377872d98daa823b6ab157d3
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-14 11:33:33 +0100

    [Debug] Debug object added to the scene.

---
+++ Kayju VS Worthless Humans/Assets/Editor/DebugEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Debug))]
+public class DebugEditor : Editor {
+    // ----------------------------------------------------------------------------
+    // Public Methods
+    // ----------------------------------------------------------------------------
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Clear")) {
+            Debug d = (Debug)target;
+                
+            d.example = false;
+        }
+    }
+}
---
+++ Kayju VS Worthless Humans/Assets/Scripts/Debug.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Debug : MonoBehaviour  {
+    // ----------------------------------------------------------------------------
+    // Configuration
+    // ----------------------------------------------------------------------------
+
+    // Gameplay
+    [Tooltip("Debug example")]
+    public bool example;
+
+    // ----------------------------------------------------------------------------
+    // Singleton
+    // ----------------------------------------------------------------------------
+
+    public static Debug instance { get; private set; }
+
+    public void Awake() {
+        instance = this;
+    }
+}
