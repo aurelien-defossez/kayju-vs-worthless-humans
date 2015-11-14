1adc15cc24b02cd3ecfb24aecd34552de5b213af
GhostOfPQ <ghostofpq@gmail.com>
2015-11-14 22:08:37 +0100

    [Global] Auto resize

--- Kayju VS Worthless Humans/Assets/Scripts/Background.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/Background.cs
@@ -2,7 +2,18 @@
 using System.Collections;
 
 public class Background : MonoBehaviour {
+    float worldScreenHeight;
+    float worldScreenWidth;
+    void Start() {
+        worldScreenHeight = (float)(Camera.main.orthographicSize * 2.0);
+        worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        Sprite sp = GetComponent<SpriteRenderer>().sprite;
+        float width = sp.texture.width / sp.pixelsPerUnit;
+        float height = sp.texture.height / sp.pixelsPerUnit;
+        gameObject.transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+
+    }
     void OnBecameInvisible() {
-        gameObject.transform.position = new Vector3(0,10,10);
+        gameObject.transform.position = new Vector3(0, 10, 10);
     }
 }
--- Kayju VS Worthless Humans/Assets/Scripts/BackgroundManager.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/BackgroundManager.cs
@@ -10,12 +10,15 @@ public class BackgroundManager : MonoBehaviour {
     Transform background;
     Transform background2;
     void Start() {
+        float worldScreenHeight = (float)(Camera.main.orthographicSize * 2.0);
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
         background = (Transform)Instantiate(backgroundPrefab);
         background.parent = scene;
         background.position = new Vector3(0, 0, 10);
         background2 = (Transform)Instantiate(backgroundPrefab);
         background2.parent = scene;
-        background2.position = new Vector3(0, 10, 10);
+        background2.position = new Vector3(0, worldScreenHeight, 10);
     }
     void Update() {
         scene.position = new Vector3(scene.position.x, scene.position.y - scrollingSpeed * Time.deltaTime, 0);
