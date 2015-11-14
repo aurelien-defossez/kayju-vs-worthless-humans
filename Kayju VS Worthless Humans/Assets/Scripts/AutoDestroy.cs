fd0df25c12a58ab9fb2ae10b93a6ae4dc631dc7b
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-14 21:13:50 +0100

    [Laser] Laser traces are destroyed when out of the screen. Bloodstains too.

---
+++ Kayju VS Worthless Humans/Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour {
+	public bool parent;
+
+	void OnBecameInvisible() {
+		Destroy(parent ? transform.parent.gameObject : gameObject);
+	}
+}
--- Kayju VS Worthless Humans/Assets/Scripts/Background.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/Background.cs
@@ -2,20 +2,7 @@
 using System.Collections;
 
 public class Background : MonoBehaviour {
-    bool hasBeenSeen;
-    void Start() {
-        hasBeenSeen = false;
-    }
-
-    void OnBecameVisible() {
-        hasBeenSeen = true;
-    }
-
     void OnBecameInvisible() {
-        if (hasBeenSeen) {
-            Debug.Log(gameObject.transform.position);
-            gameObject.transform.position = new Vector3(0,10,10);
-            Debug.Log(gameObject.transform.position);
-        }
+        gameObject.transform.position = new Vector3(0,10,10);
     }
 }
