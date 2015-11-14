24c776b024bd3464f7ce963a2a6b53d3982e4e99
GhostOfPQ <ghostofpq@gmail.com>
2015-11-14 12:01:32 +0100

    [Cursos] Axis OK for Kaiju

--- Kayju VS Worthless Humans/Assets/Scripts/KaijuCursor.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/KaijuCursor.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class KaijuCursor : MonoBehaviour {
+public class KaijuCursor : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetButtonDown("Kaiju_Stomp"))
         {
             Debug.Log("Kaiju_Stomp");
@@ -25,6 +28,22 @@ public class KaijuCursor : MonoBehaviour {
         if (Input.GetButtonDown("Kaiju_Fire"))
         {
             Debug.Log("Kaiju_Fire");
+        }
+        if (Input.GetAxis("Horizontal_Kaiju") > 0)
+        {
+            Debug.Log("Right");
+        }
+        if (Input.GetAxis("Horizontal_Kaiju") < 0)
+        {
+            Debug.Log("Left");
+        }
+        if (Input.GetAxis("Vertical_Kaiju") > 0)
+        {
+            Debug.Log("Up");
+        }
+        if (Input.GetAxis("Vertical_Kaiju") < 0)
+        {
+            Debug.Log("Down");
         }
     }
 }
