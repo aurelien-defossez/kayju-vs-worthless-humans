cb38e459a7e9f0ae7e5f001f3d40c8337d364253
GhostOfPQ <ghostofpq@gmail.com>
2015-11-14 18:40:15 +0100

    [StaminaBars] des barres mec

--- Kayju VS Worthless Humans/Assets/Scripts/KaijuCursor.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/KaijuCursor.cs
@@ -1,66 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class KaijuCursor : MonoBehaviour
-{
+public class KaijuCursor : MonoBehaviour {
     Rigidbody2D body;
     public float speed;
-	public float laserSpeedModifier;
+    public float laserSpeedModifier;
+
     public Transform stompPrefab;
     public Transform bilePrefab;
-	public Transform laserPrefab;
-	
-	Laser laser;
+    public Transform laserPrefab;
 
-	// Use this for initialization
-	void Start() {
+    public StaminaBar stompStamina;
+    public StaminaBar bileStamina;
+    public StaminaBar laserStamina;
+    public StaminaBar fireStamina;
+
+    Laser laser;
+
+    // Use this for initialization
+    void Start() {
         body = GetComponent<Rigidbody2D>();
     }
 
-	// Update is called once per frame
-	void Update() {
-		if (!KaijuDebug.instance.firstPlayerIsHuman) {
-			// BOOM
-			if (Input.GetButtonDown("Kaiju_Stomp")) {
-				Debug.Log("Kaiju_Stomp");
-				Transform stomp = (Transform)Instantiate(stompPrefab);
-				stomp.position = this.transform.position;
-			}
-
-			// LASER!!!
-			if (Input.GetButtonDown("Kaiju_Laser")) {
-				Debug.Log("Kaiju_Laser");
-				Transform laserTransform = (Transform)Instantiate(laserPrefab);
-				laser = (Laser)laserTransform.GetComponent<Laser>();
-			}
-
-			if (laser != null) {
-				laser.setPosition(this.transform.position);
-			}
-
-			if (Input.GetButtonUp("Kaiju_Laser")) {
-				laser.Kill();
-				laser = null;
-			}
-
-			// EEXCCUUUUUSSE MEEEE!
-			if (Input.GetButtonDown("Kaiju_Bile")) {
-				Debug.Log("Kaiju_Bile");
-                Transform bile = (Transform)Instantiate(bilePrefab);
-                bile.position = this.transform.position;
+    // Update is called once per frame
+    void Update() {
+        if (!KaijuDebug.instance.firstPlayerIsHuman) {
+            // BOOM
+            if (Input.GetButtonDown("Kaiju_Stomp")) {
+                if (stompStamina.getValue() >= 1f) {
+                    Debug.Log("Kaiju_Stomp");
+                    Transform stomp = (Transform)Instantiate(stompPrefab);
+                    stomp.position = this.transform.position;
+                    stompStamina.use(1f);
+                }
+                else {
+                    Debug.Log("You've not enough minerals");
+                }
             }
 
-			// KAIJU FIRE!
-			if (Input.GetButtonDown("Kaiju_Fire")) {
-				Debug.Log("Kaiju_Fire");
-			}
+            // LASER!!!
+            if (Input.GetButtonDown("Kaiju_Laser") && laserStamina.getValue() > 0.1f) {
+                Debug.Log("Kaiju_Laser");
+                Transform laserTransform = (Transform)Instantiate(laserPrefab);
+                laser = (Laser)laserTransform.GetComponent<Laser>();
+            }
 
-			Vector2 direction = new Vector2(Input.GetAxis("Horizontal_Kaiju"), Input.GetAxis("Vertical_Kaiju"));
+            if (laser != null) {
+                laser.setPosition(this.transform.position);
+                laserStamina.use(Time.deltaTime * 0.3f);
+            }
+
+            if (Input.GetButtonUp("Kaiju_Laser") || (laser != null && laserStamina.getValue() <= 0)) {
+                laser.Kill();
+                laser = null;
+            }
+
+            // EEXCCUUUUUSSE MEEEE!
+            if (Input.GetButtonDown("Kaiju_Bile")) {
+                if (bileStamina.getValue() > 0.5f) {
+                    Debug.Log("Kaiju_Bile");
+                    Transform bile = (Transform)Instantiate(bilePrefab);
+                    bile.position = this.transform.position;
+                    bileStamina.use(0.5f);
+                }
+                else {
+                    Debug.Log("You require more vespene gaz");
+                }
+            }
+
+            // KAIJU FIRE!
+            if (Input.GetButtonDown("Kaiju_Fire") && fireStamina.getValue() > 0.1f) {
+                Debug.Log("Kaiju_Fire");
+            }
+
+            Vector2 direction = new Vector2(Input.GetAxis("Horizontal_Kaiju"), Input.GetAxis("Vertical_Kaiju"));
             body.velocity = direction * speed;
 
-			if (laser != null) {
-				body.velocity *= laserSpeedModifier;
-			}
+            if (laser != null) {
+                body.velocity *= laserSpeedModifier;
+            }
         }
-	}
+    }
 }
---
+++ Kayju VS Worthless Humans/Assets/StaminaBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaminaBar : MonoBehaviour {
+    public float reloadSpeed;
+    public float value;
+
+    // Use this for initialization
+    void Start() {
+        value = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (value < 1f) {
+            value += Mathf.Min(1f, Time.deltaTime * reloadSpeed);
+        }
+    }
+
+    public void use(float qty) {
+        value = Mathf.Max(0, value - qty);
+    }
+
+    public float getValue() {
+        return value;
+    }
+}
