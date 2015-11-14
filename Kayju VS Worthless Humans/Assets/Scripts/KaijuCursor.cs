c8aa64ad69edc5ad54afa2a6437cdfd2d75da703
GhostOfPQ <ghostofpq@gmail.com>
2015-11-14 11:19:51 +0100

    input OK for Kaiju actions

---
+++ Kayju VS Worthless Humans/Assets/Scripts/KaijuCursor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KaijuCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Kaiju_Stomp"))
+        {
+            Debug.Log("Kaiju_Stomp");
+        }
+        if (Input.GetButtonDown("Kaiju_Laser"))
+        {
+            Debug.Log("Kaiju_Laser");
+        }
+        if (Input.GetButtonDown("Kaiju_Bile"))
+        {
+            Debug.Log("Kaiju_Bile");
+        }
+        if (Input.GetButtonDown("Kaiju_Fire"))
+        {
+            Debug.Log("Kaiju_Fire");
+        }
+    }
+}
