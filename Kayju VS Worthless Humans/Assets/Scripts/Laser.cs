ce8377b278c7c7972bd1694c4c591348d7a8b10d
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-14 20:38:17 +0100

    [Laser] Laser traces added.

--- Kayju VS Worthless Humans/Assets/Scripts/Laser.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/Laser.cs
@@ -3,9 +3,15 @@
 public class Laser : MonoBehaviour {
 	public Transform laser;
 	public Vector3 origin;
+	public Transform laserTracePrefab;
 
 	public void setPosition(Vector2 position) {
 		transform.position = new Vector3(position.x, position.y, 0);
+
+		Transform trace = (Transform)Instantiate(laserTracePrefab);
+		trace.position = transform.position;
+		trace.rotation = Quaternion.Euler(0, 0, Random.value * 360);
+
 		Rotate();
 	}
 
