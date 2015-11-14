7bfbe247d34bc3ae0afc15e58d7276d605f7a8a1
GhostOfPQ <ghostofpq@gmail.com>
2015-11-14 20:06:12 +0100

    [BAckground] infiniteBG

---
+++ Kayju VS Worthless Humans/Assets/Scripts/Background.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+    public float scrollingSpeed;
+    bool hasBeenSeen;
+    void Start() {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1) * scrollingSpeed;
+        hasBeenSeen = false;
+    }
+
+    void OnBecameVisible() {
+        hasBeenSeen = true;
+    }
+
+    void OnBecameInvisible() {
+        if (hasBeenSeen) {
+            gameObject.transform.position = new Vector3(0, 10, 10);
+        }
+    }
+}
---
+++ Kayju VS Worthless Humans/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour {
+    public Transform backgroundPrefab;
+    // Use this for initialization
+    Transform background;
+    Transform background2;
+    void Start() {
+        background = (Transform)Instantiate(backgroundPrefab);
+        background.position = new Vector3(0, 0, 10);
+        background2 = (Transform)Instantiate(backgroundPrefab);
+        background2.position = new Vector3(0, 10, 10);
+    }
+}
