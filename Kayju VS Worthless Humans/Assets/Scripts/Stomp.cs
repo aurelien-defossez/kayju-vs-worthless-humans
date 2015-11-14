b8d14d6bd424150db0bfc3f0c565b7a7c570f18d
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-14 12:59:05 +0100

    [Stomp] Stomp prefab created.

---
+++ Kayju VS Worthless Humans/Assets/Scripts/Stomp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Stomp : MonoBehaviour {
+    public Transform shadow;
+    public float maxSize;
+    public Timeline timeline;
+    public Range shadowOpacity;
+
+    private SpriteRenderer shadowSprite;
+
+    public void Start() {
+        shadowSprite = shadow.GetComponent<SpriteRenderer>();
+
+        timeline.Play();
+    }
+
+    [TimelineMethod]
+    public void GrowShadow(TimelineCall options) {
+        shadow.localScale = new Vector2(1, 1) * options.progress * maxSize;
+        shadowSprite.color = shadowSprite.color.A(shadowOpacity.Lerp(options.progress));
+    }
+}
