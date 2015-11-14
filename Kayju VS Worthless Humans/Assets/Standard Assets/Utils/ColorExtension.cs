abb6a0546d67125865c9592cf08e8f7550db308f
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-14 12:58:01 +0100

    [Utils] Some utils methods added.

---
+++ Kayju VS Worthless Humans/Assets/Standard Assets/Utils/ColorExtension.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public static class ColorExtension {
+    public static Color A(this Color c, float a) {
+        c.a = a;
+        return c;
+    }
+}
---
+++ Kayju VS Worthless Humans/Assets/Standard Assets/Utils/Range.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Range {
+    public float from;
+    public float to;
+
+    public float Lerp(float delta) {
+        return Mathf.Lerp(from, to, delta);
+    }
+
+    public float InverseLerp(float value) {
+        return Mathf.InverseLerp(from, to, value);
+    }
+    public float Rand() {
+        return UnityEngine.Random.Range(from, to);
+    }
+}
+
+[System.Serializable]
+public class IntRange {
+    public int from;
+    public int to;
+
+    public int Rand() {
+        return UnityEngine.Random.Range(from, to + 1);
+    }
+}
