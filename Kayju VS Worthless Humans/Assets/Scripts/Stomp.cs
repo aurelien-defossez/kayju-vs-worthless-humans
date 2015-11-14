ef30c1cf6f2cc19b3c860c9efc6b460b6afd0e2a
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-14 16:34:55 +0100

    [Stomp] Fist integrated.

--- Kayju VS Worthless Humans/Assets/Scripts/Stomp.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/Stomp.cs
@@ -5,31 +5,42 @@ using System.Collections.Generic;
 
 public class Stomp : MonoBehaviour {
     public Transform shadow;
+	public Transform fist;
     public float maxSize;
     public Timeline timeline;
     public Range shadowOpacity;
     public CircleCollider2D hitCollider;
     public CircleCollider2D blockCollider;
-
+	public Vector2 initialFistPosition;
 
     private SpriteRenderer shadowSprite;
 
     public void Start() {
         shadowSprite = shadow.GetComponent<SpriteRenderer>();
+		initialFistPosition = fist.position;
 
         timeline.Play();
-    }
+	}
 
-    [TimelineMethod]
-    public void GrowShadow(TimelineCall options) {
-        shadow.localScale = new Vector2(1, 1) * options.progress * maxSize;
-        shadowSprite.color = shadowSprite.color.A(shadowOpacity.Lerp(options.progress));
-    }
-    [TimelineMethod]
+	[TimelineMethod]
+	public void GrowShadow(TimelineCall options) {
+		shadow.localScale = new Vector2(1, 1) * options.progress * maxSize;
+		shadowSprite.color = shadowSprite.color.A(shadowOpacity.Lerp(options.progress));
+	}
+
+	[TimelineMethod]
+	public void MoveFist(TimelineCall options) {
+		fist.position = Vector2.Lerp(initialFistPosition, transform.position,
+			options.parameter == 0 ? options.progress : 1 - options.progress);
+	}
+
+	[TimelineMethod]
     public void Hit(TimelineCall options)
     {
         hitCollider.enabled=true;
-    }
+		shadowSprite.enabled = false;
+	}
+
     [TimelineMethod]
     public void HitEnd(TimelineCall options)
     {
