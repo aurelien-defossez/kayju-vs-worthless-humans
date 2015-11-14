2a88b2ae5bc7c25d204e984843a33183dd90c130
Alkhan797 <Alkhan797@gmail.com>
2015-11-14 21:56:43 +0100

    Score printing

--- Kayju VS Worthless Humans/Assets/Scripts/ScoringBoard.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/ScoringBoard.cs
@@ -5,18 +5,27 @@ using System.Collections.Generic;
 public class ScoringBoard : MonoBehaviour {
 
     public Dictionary<string, int> score;
+    public GUIText scoreText;
+    int stext;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         score = new Dictionary<string, int>();
         score["Kaiju"] = 0;
         score["J2"] = 0;
         score["J3"] = 0;
         score["J4"] = 0;
-	}
+        stext = 0;
+        scoreText.text = "LORD LIZARD DESTROY NONE OF THOSE WORTHLESS HUMANS !!";
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("Kaiju score : [" + score["Kaiju"] +"]");
-	}
+        if (stext != score["Kaiju"])
+        {
+            stext = score["Kaiju"];
+            scoreText.text = "LORD LIZARD DESTROY " + stext + " OF THOSE WORTHLESS HUMANS !!";
+            Debug.Log("SCORE UPDATED !!! SCORE IS NOW OF " + stext);
+        }
+    }
 }
