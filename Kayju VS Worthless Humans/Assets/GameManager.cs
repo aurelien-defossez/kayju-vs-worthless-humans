fa1f2c5fab47f2f0f230adce869f149a9504029d
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-15 15:30:08 +0100

    [Refactoring] Player management is now better.

--- Kayju VS Worthless Humans/Assets/GameManager.cs
+++ Kayju VS Worthless Humans/Assets/GameManager.cs
@@ -5,37 +5,28 @@ public class GameManager : MonoBehaviour {
 
     public Human humanPrefab;
     public Transform scene;
+	public int maxNbPlayers;
+	public int firstPlayerIndex;
+	public Range spawnRange;
 
-    Human player2;
-    Human player3;
-    Human player4;
+	Human[] players;
 
     // Use this for initialization
     void Start() {
+		players = new Human[maxNbPlayers];
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetButtonDown("Action_J2") && player2 == null) {
-            Debug.Log("Action_J2");
-            player2 = Instantiate(humanPrefab);
-            player2.transform.position = new Vector3(-1, 0, 0);
-            player2.transform.SetParent(scene);
-            player2.SetPlayer(2);
-        }
-        if (Input.GetButtonDown("Action_J3") && player3 == null) {
-            Debug.Log("Action_J3");
-            player3 = Instantiate(humanPrefab);
-            player3.transform.position = new Vector3(0, 0, 0);
-            player3.transform.SetParent(scene);
-            player3.SetPlayer(3);
-        }
-        if (Input.GetButtonDown("Action_J4") && player4 == null) {
-            Debug.Log("Action_J4");
-            player4 = Instantiate(humanPrefab);
-            player4.transform.position = new Vector3(1, 0, 0);
-            player4.transform.SetParent(scene);
-            player4.SetPlayer(4);
-        }
+		for (int i = 0; i < maxNbPlayers; i++) {
+			int index = (firstPlayerIndex + i);
+            if (players[i] == null && Input.GetButtonDown("Action_J" + index)) {
+				Debug.Log("Player " + index + " connected.");
+				Human player = Instantiate(humanPrefab);
+				player.transform.position = new Vector3(spawnRange.Rand(), 0, 0);
+				player.transform.SetParent(scene);
+				player.SetPlayer(2);
+			}
+		}
     }
 }
