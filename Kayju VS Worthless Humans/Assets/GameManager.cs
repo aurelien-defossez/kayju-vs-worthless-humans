ad270f27e9d30b8d1040782753adbec6a89b3bfb
GhostOfPQ <ghostofpq@gmail.com>
2015-11-15 15:44:09 +0100

    generate bobies

--- Kayju VS Worthless Humans/Assets/GameManager.cs
+++ Kayju VS Worthless Humans/Assets/GameManager.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
     public Human humanPrefab;
     public Transform scene;
-	public int maxNbPlayers;
-	public int firstPlayerIndex;
-	public Range spawnRange;
+    public int maxNbPlayers;
+    public int firstPlayerIndex;
+    public Range spawnRange;
 	public AudioClip[] runSounds;
 
 	Human[] players;
 	AudioSource currentRunSound;
+	 float timer;
+    float worldScreenHeight;
+    float worldScreenWidth;
 
     // Use this for initialization
     void Start() {
+        timer = Random.Range(1, 3);
         players = new Human[maxNbPlayers];
+        worldScreenHeight = (float)(Camera.main.orthographicSize * 2.0);
+        worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        worldScreenHeight *= 0.9f;
+        worldScreenWidth *= 0.72f;
     }
 
     // Update is called once per frame
@@ -36,5 +44,19 @@ public class GameManager : MonoBehaviour {
 			currentRunSound = Utils.PlayPitchedClipAt(runSounds[0], gameObject, Random.Range(0.8f, 1.2f));
 			currentRunSound.volume = 0.2f;
         }
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0) {
+
+            Human pnj = Instantiate(humanPrefab);
+
+            pnj.transform.position = new Vector3(Random.Range(-worldScreenWidth / 2, worldScreenWidth / 2), Random.Range(-worldScreenHeight / 2, worldScreenHeight / 2), 0);
+            pnj.transform.SetParent(scene);
+
+
+            timer = Random.Range(1, 3);
+        }
+
     }
 }
