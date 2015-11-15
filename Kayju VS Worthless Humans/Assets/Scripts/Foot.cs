010bb2a68aaaa05b875c16bb416b3b512d48a252
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-15 13:05:29 +0100

    [Feet] Feet integrated.

---
+++ Kayju VS Worthless Humans/Assets/Scripts/Foot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foot : MonoBehaviour {
+	public float yLimit;
+	public Timeline timeline;
+	public AudioClip footSound;
+
+	Vector2 savedPosition;
+
+	// Update is called once per frame
+	void Update() {
+		if (transform.position.y < yLimit) {
+			savedPosition = transform.position;
+            timeline.Restart();	
+            timeline.Play();
+		}
+	}
+
+	[TimelineMethod]
+	public void MoveFoot(TimelineCall options) {
+		transform.position = savedPosition + new Vector2(0, -yLimit) * options.progress;
+	}
+
+	[TimelineMethod]
+	public void PlaySound(TimelineCall options) {
+		Utils.PlayPitchedClipAt(footSound, gameObject, Random.Range(0.8f, 1.2f));
+	}
+}
