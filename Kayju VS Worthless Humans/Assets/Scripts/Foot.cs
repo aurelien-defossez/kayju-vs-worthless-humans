8a66195c7f5407ce91dc4fa3ee027882514c5461
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-15 13:20:11 +0100

    [Feet] Feet kill!

--- Kayju VS Worthless Humans/Assets/Scripts/Foot.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/Foot.cs
@@ -5,6 +5,7 @@ public class Foot : MonoBehaviour {
 	public float yLimit;
 	public Timeline timeline;
 	public AudioClip footSound;
+	public Collider2D footCollider;
 
 	Vector2 savedPosition;
 
@@ -25,5 +26,15 @@ public class Foot : MonoBehaviour {
 	[TimelineMethod]
 	public void PlaySound(TimelineCall options) {
 		Utils.PlayPitchedClipAt(footSound, gameObject, Random.Range(0.8f, 1.2f));
+	}
+
+	[TimelineMethod]
+	public void Hit(TimelineCall options) {
+		footCollider.enabled = true;
+	}
+
+	[TimelineMethod]
+	public void StopHit(TimelineCall options) {
+		footCollider.enabled = false;
 	}
 }
