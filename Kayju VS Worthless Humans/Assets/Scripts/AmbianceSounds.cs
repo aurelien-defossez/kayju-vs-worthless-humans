f17b41eecb2da2896df3820d1856ae3c4f9b84e9
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-15 15:15:52 +0100

    [Sounds] Ambient sounds added.

---
+++ Kayju VS Worthless Humans/Assets/Scripts/AmbianceSounds.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AmbianceSounds : MonoBehaviour {
+	public AudioClip[] ambianceSounds;
+	public Range delay;
+
+	float timer;
+
+	public void Start() {
+		PlaySound();
+	}
+
+	public void Update() {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+			PlaySound();
+		}
+	}
+
+	private void PlaySound() {
+		Utils.PlayPitchedClipAt(ambianceSounds[Random.Range(0, ambianceSounds.Length)],
+			new Vector3(Random.value < 0.5 ? -10 : 10, 0, 0), Random.Range(0.8f, 1.2f));
+		timer = delay.Rand();
+	}
+}
