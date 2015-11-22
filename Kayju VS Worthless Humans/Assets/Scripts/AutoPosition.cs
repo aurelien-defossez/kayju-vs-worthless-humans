c1df2d1184eaf8954fadf1c4ed1cbbe4a82ba2a7
Aurélien Defossez <defossez.aurelien@tabemasu.com>
2015-11-22 17:26:46 +0100

    [UI] Kaiju buttons are positioned depending on the screen size.

---
+++ Kayju VS Worthless Humans/Assets/Scripts/AutoPosition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class AutoPosition : MonoBehaviour {
+    public float x;
+    public float y;
+
+    // Use this for initialization
+    void Start() {
+        float worldScreenHeight = (float)(Camera.main.orthographicSize * 2.0);
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        
+        transform.position = new Vector2(worldScreenWidth * x / 2f, worldScreenHeight * y / 2f);
+    }
+}
