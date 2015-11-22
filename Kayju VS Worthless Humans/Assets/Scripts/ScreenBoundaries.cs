5d8555e452fe0294bafd608a458e53a2d196f706
Aurélien Defossez <defossez.aurelien@tabemasu.com>
2015-11-22 17:51:12 +0100

    [Refactoring] Using Utils.GetScreenPosition in screen boundaries.

--- Kayju VS Worthless Humans/Assets/Scripts/ScreenBoundaries.cs
+++ Kayju VS Worthless Humans/Assets/Scripts/ScreenBoundaries.cs
@@ -2,33 +2,19 @@
 using System.Collections;
 
 public class ScreenBoundaries : MonoBehaviour {
-
     public BoxCollider2D colliderNorth;
     public BoxCollider2D colliderEast;
     public BoxCollider2D colliderSouth;
     public BoxCollider2D colliderWest;
 
+	public float verticalOffset;
+	public float horizontalOffset;
+
     // Use this for initialization
     void Start() {
-        float worldScreenHeight = (float)(Camera.main.orthographicSize * 2.0);
-        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
-
-        //Debug.Log(worldScreenHeight);
-        //Debug.Log(worldScreenWidth);
-
-        colliderNorth.offset = new Vector2(0, (worldScreenHeight * 0.9f / 2f));
-        colliderSouth.offset = new Vector2(0, (-worldScreenHeight * 0.9f / 2f));
-        colliderEast.offset = new Vector2((worldScreenWidth * 0.75f / 2f), 0);
-        colliderWest.offset = new Vector2((-worldScreenWidth * 0.75f / 2f), 0);
-
-        //Debug.Log(colliderNorth.offset);
-        //Debug.Log(colliderSouth.offset);
-        //Debug.Log(colliderEast.offset);
-        //Debug.Log(colliderWest.offset);
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+		colliderNorth.offset = Utils.GetScreenPosition(0, verticalOffset);
+		colliderSouth.offset = Utils.GetScreenPosition(0, -verticalOffset);
+		colliderEast.offset = Utils.GetScreenPosition(horizontalOffset, 0);
+		colliderWest.offset = Utils.GetScreenPosition(-horizontalOffset, 0);
     }
 }
