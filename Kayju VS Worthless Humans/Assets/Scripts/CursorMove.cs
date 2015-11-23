2d7c47036a5f638f108f6a247e1fc0c16358d164
Aurélien Defossez <defossez.aurelien@gmail.com>
2015-11-23 22:45:14 +0100

    [Team] Team scene created, cursor moves.

---
+++ Kayju VS Worthless Humans/Assets/Scripts/CursorMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CursorMove : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody2D body;
+
+	void Start() {
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	public void Update() {
+		Vector2 direction = new Vector2(Input.GetAxis("Horizontal_Kaiju"), Input.GetAxis("Vertical_Kaiju"));
+		body.velocity = direction * speed;
+	}
+}
